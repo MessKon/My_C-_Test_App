58dc8fd8ca7521ef0903d36fcf1715e8aae5a7c4
kostas <messiskon@gmail.com>
2019-01-29 22:47:24 +0000

    Implement MonthDay class

--- testApp/Program.cs
+++ testApp/Program.cs
@@ -18,23 +18,45 @@ namespace testApp
 	}
 
 	//class My_Input
-	//class Every
+	//might not need a class Daily
 	//class WeekDay
-	//class MonthDay
+	class MonthDay
+	{
+		internal List<string>[] mdayList;
+
+		internal MonthDay()
+		{
+			mdayList = new List<string>[28];
+			for (int i=0; i<28;i++)
+                        {
+				mdayList[i]= new List<string>();
+			}
+		}
+
+		internal void dateAdd(int date,string name)
+		{
+			mdayList[date].Add(name);
+		}
+
+		internal string getString(int date)
+		{
+			return String.Join(", ", mdayList[date].ToArray());
+		}
+	}
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			var names = new List<string> { "<name>", "Ana", "Felipe" };
-			//foreach (var name in names)
-			//{
-			//	Console.WriteLine($"Hello {name.ToUpper()}!");
-			//}
+			List<string> DailyList = new List<string>();
+
+			MonthDay MDay = new MonthDay();
+			MDay.dateAdd(6,"Bingo Cust");
+			MDay.dateAdd(6,"Zumba Cust");
+			MDay.dateAdd(5,"Super Cust");
 			string output= "";
 			for (int i=4; i<12;i++)
 			{
-				output += DateTime.Now.AddDays(i).ToString("ddd, dd MMMM yyyy") + ": some customers" + System.Environment.NewLine;
-				//Console.WriteLine(DateTime.Now.AddDays(i).ToString("ddd, dd MMMM yyyy") + ": some customers");
+				output += DateTime.Now.AddDays(i).ToString("ddd, dd MMMM yyyy") + ": "+  MDay.getString(i) + System.Environment.NewLine;
 			}
 			//My_Output.Print_console(output);
 			My_Output.Print_file(output);
